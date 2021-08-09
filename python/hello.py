93f78de21663c377d109a013bb2fd88b0feda1c1
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-08-09 16:26:15 +0530

    print and input function

---
+++ python/hello.py
@@ -0,0 +1,27 @@
+x = 5
+y = 10
+z = x + y
+
+print(x)
+print(y)
+
+#Single line comment
+
+'''
+Data Types:
+- int: numbers
+- float: decimal numbers
+- string: everything in between inverted commas
+- boolean: True/False
+'''
+
+x = "Hello"
+y = 'How are you?'
+
+print(x, y)
+print(x, y, sep="\t", end="")
+
+#By default, input stores values in string format
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+percentage = float(input("Enter your percentage: "))
